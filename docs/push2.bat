75517b3ac5828a4bfcc5923f96d646a533706d9c
juoyo <juoyohub@gmail.com>
2024-04-06 22:15:56 +0800

    ''

--- docs/push2.bat
+++ docs/push2.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start cmd /k "cd F: && cd F:\BookHub\gkbook && gitbook build ./ ./docs && timeout /t 5 /nobreak >nul && git add *&& set now=%date% %time%&& git commit -m '%now%' && git push && pause"
+start cmd /k "cd F: && cd F:\BookHub\gkbook && gitbook build ./ ./docs && timeout /t 3 /nobreak >nul && git add *&& set now=%date% %time%&& git commit -m '%now%' && git push && pause"
 
